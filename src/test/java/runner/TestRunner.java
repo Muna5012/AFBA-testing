272f975e36a00cfbf388624c3423087f58e1e27f
MAbabaikeli <MAbabaikeli@afba.com>
2019-09-19 17:51:26 -0400

    updated git ignore

--- src/test/java/runner/TestRunner.java
+++ src/test/java/runner/TestRunner.java
@@ -21,7 +21,7 @@ import cucumber.api.junit.Cucumber;
 		,glue = "stepdefination",
 		dryRun = false,
 		//tags = {"@Test05","@Test06","@Test07"}
-		tags = {"@Test01"}
+		tags = {"@Test21"}
 		
 		)
 
