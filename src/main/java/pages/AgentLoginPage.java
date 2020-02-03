c94cda2c4194538ec988562230ecc2a2926ae0ca
MAbabaikeli <MAbabaikeli@afba.com>
2020-02-03 16:24:24 -0500

    update

---
+++ src/main/java/pages/AgentLoginPage.java
@@ -0,0 +1,22 @@
+package pages;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class AgentLoginPage {
+
+	@FindBy(xpath="//*[contains(text(),'Agent Login')]")
+	public static WebElement AgentLoginBtn;
+	
+	@FindBy(xpath="//*[@id='username']")
+	public static WebElement InputUserName;
+	
+	@FindBy(xpath="//*[@id='password']")
+	public static WebElement InputPassword;
+	
+	@FindBy(className = "btn")
+	public static WebElement LoginBtn;
+	
+	@FindBy(xpath="//*[contains(text(),'Forgot your password?')]")
+	public static WebElement ForgetUserNameBtn;
+}
