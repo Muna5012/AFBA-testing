70ee7d885acbcae1a2a1fd42eb02b7ac462239b3
MAbabaikeli <MAbabaikeli@afba.com>
2020-03-06 17:41:57 -0500

    update

--- src/test/java/runner/TestRunner.java
+++ src/test/java/runner/TestRunner.java
@@ -20,7 +20,7 @@ import cucumber.api.junit.Cucumber;
 		features = "EappFeatures"
 		,glue = "EappStepDefination",
 		dryRun = false,
-		tags = {"@callCenter1"}
+		tags = {"@membershipApplicationverify"}
 		)
 
 public class TestRunner {
